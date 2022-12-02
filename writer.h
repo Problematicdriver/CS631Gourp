22429bcd2db2db9dd92ae797b528eea7d00ce427
3santree <wangyueffect@163.com>
2022-12-02 16:29:19 -0500

    Add writer.c

--- writer.h
+++ writer.h
@@ -1,5 +1,8 @@
 #include <strings.h>
+#include <dirent.h>
+
 #include "sws.h"
+
 typedef struct response
 {
     char* http_version;
@@ -11,8 +14,10 @@ typedef struct response
     char* content_type;
     char* content_length;
     char* body;
-};
+} repsonse;
 
 void writer(char* err, int client_fd);
-char* response_string(struct response r);
-bool send_all(int socket, void *buffer, size_t length);
+void send_response(int client_fd, void *response, size_t length);
+char* r_body(char* path);
+char* file_content(char* path);
+char* dir_content(char* path);
